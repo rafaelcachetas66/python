81974fb66f0a2062b5b47a7e2821feee45157c36
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-19 21:58:31 +0000

    Finished Lesson 8

---
+++ software/day_1/lesson_8/main.py
@@ -0,0 +1,17 @@
+# Write a program in main.py that prints some notes on Python
+# programming into the Output console.
+
+# Warning: The output in your program should match the example output shown below
+# exactly, character for character, even spaces and symbols should be identical, otherwise 
+# the tests won't pass
+
+# EXAMPLE:
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# Write the code below
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
