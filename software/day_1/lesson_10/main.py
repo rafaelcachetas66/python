2ee14dd9c744aa5b9571be7536ad90c24b200142
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-19 22:29:52 +0000

    Finished Lesson 10

---
+++ software/day_1/lesson_10/main.py
@@ -0,0 +1,27 @@
+# INSTRUCTIONS:
+# Look at the code in the code editor. There are errors in all of the lines of code. Fix the code
+# so that it runs without errors.
+
+# Warning: The output in your program should match the example output shown below exactly, character 
+# for character, even space and symbols should be identical, otherwise the tests won't pass.
+
+# EXAMPLE:
+# When you run your program, it should print the following:
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New line can be created with a backslash and n.
+
+# Fix the code:
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello" + "world")')
+# print(("New lines can be created with a backslash and n.'))
+
+# Write the code below:
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New line can be created with a backslash and n.")
