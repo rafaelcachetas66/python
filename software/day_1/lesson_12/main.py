71110e9ce7bff6082e7363cfe04fd22c3d219608
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-19 22:57:06 +0000

    Finished Lesson 12

---
+++ software/day_1/lesson_12/main.py
@@ -0,0 +1,10 @@
+# INSTRUCTIONS:
+# Write a program that print the number of characters in a name.
+# Warning: Your program should work for different inputs. e.g. any name that you input.
+# NOTE: You can access the value in the input pane using the input() function
+
+# Write the code below:
+
+s = input() # Get the user input
+number_of_char = len(s) # Count the characters of the input and store it in a variable
+print(number_of_char) # Print the number of characters
