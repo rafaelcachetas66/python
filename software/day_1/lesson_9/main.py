81a0f799a79b735d57e0f449153d3a2939b35484
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-19 22:09:26 +0000

    Finished Lesson 9

---
+++ software/day_1/lesson_9/main.py
@@ -0,0 +1,11 @@
+# Using the '\n' the code changes lines, so it can be used to save print resources
+
+print("Hello World!\nHello World!\nHello World!")
+
+# It is possible to concatinate strings using the '+'
+
+print("Hello" + " " + "Rafael")
+print("Hello" + " Rafael")
+
+# Spaces are used for identation, be careful with the TAB at the beginning of the code
+
