7b28ced810521669afce80ee8c4b41484cc3427a
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-19 22:39:10 +0000

    Finished Lesson 11

---
+++ software/day_1/lesson_11/main.py
@@ -0,0 +1,6 @@
+# input(prompt) passes the data, print(prompt) only prints the data
+
+print("Hello " + input("What is your name?") + "!")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
