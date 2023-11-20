5ec19e1bd500a88f1bea3be3eb49e0b3464f6428
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-20 20:58:49 +0000

    Finished Lesson 13

---
+++ software/day_1/lesson_13/main.py
@@ -0,0 +1,16 @@
+# PYTHON VARIABLES
+
+name = input("What is your name?") # Saves the input to a variable
+print(name) # Prints the variable
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+print(len(input("What is your name")))
+
+name = input("What is your name")
+length = len(name)
+print(length)
