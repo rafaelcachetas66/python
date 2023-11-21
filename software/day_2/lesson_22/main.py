eaf4ca9ea1d76e2bddec6b2e3da40e905ac9c1fe
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-21 21:32:08 +0000

    Finished Lesson 22

---
+++ software/day_2/lesson_22/main.py
@@ -0,0 +1,18 @@
+# MATHEMATICAL OPERATIONS IN PYTHON
+
+3 + 5 # Addition (2nd)
+7 - 2 # Subtraction (2nd)
+3 * 2 # Multiplication (1st)
+6 / 3 # Division (1st)
+
+print(6 / 3) # Division result is a float
+
+2 ** 2 # Exponential (1st)
+
+print(2 ** 3)
+
+# PEMDAS => Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+# ()
+# **
+# * /
+# + -
