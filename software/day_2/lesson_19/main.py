e6a49991a89e563e74c544ab8ff32feef9cee56f
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-21 20:10:16 +0000

    Finished Lesson 19

---
+++ software/day_2/lesson_19/main.py
@@ -0,0 +1,22 @@
+# DATA TYPES
+
+# STRING
+
+print("Rafael"[0]) # Subscript (pulling from an array)
+
+# INTEGER
+
+s = 123
+
+print(s + 405)
+
+# FLOAT
+
+y = 123.5
+
+print(y + 5)
+
+# BOOLEAN
+
+True
+False
