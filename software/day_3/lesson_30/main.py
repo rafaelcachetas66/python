14a1bc1c2710bdd60afe176776fd961e86eea34b
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-23 12:27:44 +0000

    Finished Lesson 31

--- software/day_3/lesson_30/main.py
+++ software/day_3/lesson_30/main.py
@@ -9,10 +9,19 @@
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height? "))
-if (height > 120):
+if (height > 120): # It does not include 120
     print("Feel free to join!")
 else:
     print("Go back!")
     
 # WARNING: Careful with the identation level
 # What is idented is inside a block of code
+
+# COMPARISON OPERATORS 
+
+# < Less than
+# > Greater than
+# <= Less than or equal to
+# >= Greater than or equal to
+# == Equal to
+# != Not equal to
---
+++ software/day_3/lesson_31/main.py
@@ -0,0 +1,13 @@
+# CODING EXERCISE
+
+# Check if a number is odd or even based on the user input.
+
+# Which number do you want to check?
+number = int(input())
+
+# Write your code below this line
+
+if (number % 2 == 0):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
