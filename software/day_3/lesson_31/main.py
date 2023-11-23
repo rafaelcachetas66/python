6f203e3c27c30cbafed5343cfd10b666fae17f91
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-23 12:41:49 +0000

    Finished Lesson 32

--- software/day_3/lesson_31/main.py
+++ software/day_3/lesson_31/main.py
@@ -7,7 +7,7 @@ number = int(input())
 
 # Write your code below this line
 
-if (number % 2 == 0):
+if (number % 2 == 0): # if it can be divided by 2, then it is even
     print(f"{number} is even.")
 else:
     print(f"{number} is odd.")
