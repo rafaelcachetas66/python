362922a34e505c73bc2cf95038491e057800ab22
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-23 10:54:11 +0000

    Finished Lesson 30

---
+++ software/day_3/lesson_30/main.py
@@ -0,0 +1,18 @@
+# CONTROL FLOW WITH IF/ELSE AND CONDITIONAL OPERATORS
+
+#water_level = 50
+
+#if(water_level > 80):
+#    print("Overflow")
+#else:
+#    print("All good")
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height? "))
+if (height > 120):
+    print("Feel free to join!")
+else:
+    print("Go back!")
+    
+# WARNING: Careful with the identation level
+# What is idented is inside a block of code
