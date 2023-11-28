bbe1b144d2cb21847815760ae5ca6ddd627f6dc2
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-28 20:24:57 +0000

    Finished Lesson 46

---
+++ software/day_4/lesson_46/main.py
@@ -0,0 +1,10 @@
+# INDEX ERRORS AND WORKED WITH NESTED LISTS
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# How to create a list within a list? (nested)
+
+dirty_doze = [fruits, vegetables]
+
+print(dirty_doze)
