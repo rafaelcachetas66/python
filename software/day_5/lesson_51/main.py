d73d9c72c5d721ea63878fd796a1b4b6aee72d5b
rafaelcachetas66 <rafaelcachetas66@gmail.com>
2023-11-30 15:53:57 +0000

    Finished Lesson 51

---
+++ software/day_5/lesson_51/main.py
@@ -0,0 +1,12 @@
+# USING THE FOR LOOPS WITH PYTHON LISTS
+
+# For Loop
+
+# for item in list_of_items:
+    # do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
